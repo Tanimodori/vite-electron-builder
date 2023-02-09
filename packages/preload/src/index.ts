79ee09cee40bf5bd4468d068327c38653198af11
Tanimodori <unknowner2014@gmail.com>
2023-02-09 16:53:08 +0800

    test: add from thorn0/rollup-commonjs-semver-issue

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -2,5 +2,9 @@
  * @module preload
  */
 
+const semver = require('semver');
+
+console.log(semver.satisfies('4.2.4', '>= 4.0.0'));
+
 export {sha256sum} from './nodeCrypto';
 export {versions} from './versions';
