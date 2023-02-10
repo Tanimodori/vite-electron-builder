83c84ecdae2c311c4a51f27b551fec15b07ebe6a
Tanimodori <unknowner2014@gmail.com>
2023-02-10 09:08:39 +0800

    test: add code from #780

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,4 +1,7 @@
 import {createApp} from 'vue';
 import App from '/@/App.vue';
 
+import {create} from 'ipfs-http-client';
+export const ipfs = create({host: 'localhost', port: 5001, protocol: 'http'});
+
 createApp(App).mount('#app');
